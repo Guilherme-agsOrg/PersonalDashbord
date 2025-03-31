4109c4b91611b928279651d8b0dd7ce5e64ae11e
Guilherme <65544746+Guilherme-ags@users.noreply.github.com>
2025-03-31 19:26:35 -0300

    front: Setting up the default page
    - Setting up the default page;

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,10 @@
+'use client'
+
 
 export default function Home() {
   return (
-    <div>
-      Hello World
+    <div className="flex flex-col h-screen bg-zinc-100 text-black">
+
     </div>
   );
 }

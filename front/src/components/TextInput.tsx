dcbc45ce437e601b7822eb9278dc8fd805db4f22
Guilherme <65544746+Guilherme-ags@users.noreply.github.com>
2025-04-07 20:03:37 -0300

    front: Non-functional login and registration completed
    - Creating the finished non-functional login and registration page design;

--- front/src/components/TextInput.tsx
+++ front/src/components/TextInput.tsx
@@ -2,8 +2,7 @@ import { InputHTMLAttributes, ReactElement } from "react"
 
 export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement>{
     type: string
-    text: string
-    onChange: () => void
+    value: string
     label?: string
     minLength?: number
     maxLength?: number
@@ -11,27 +10,29 @@ export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement>{
     icon?: ReactElement
     iconLeft?: boolean
     error?: string
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 export default function TextInput({ label, icon, iconLeft, error, ...props }: TextInputProps){
     return (
-        <div className="
-            flex flex-col items-start justify-center p-2 w-3/12
-        ">
+        <div className={`
+            flex flex-col justify-center p-2
+            `
+        }>
             {label && 
-                <label className="text-stone-900 text-sm">
+                <label className="text-[var(--color-amethystSmoke-800)] text-sm font-semibold">
                     {label}
                 </label>
             }
 
-            <div className="
-                flex gap-2 rounded-lg p-2
-                bg-[var(--color-amethystSmoke-200)] text-stone-900
-                focus-within:bg-[var(--color-amethystSmoke-300)]
-            ">
-                {(icon && iconLeft)&& <span className="text-stone-900">{icon}</span>}
+            <div className={`
+                flex rounded-lg p-2
+                bg-[var(--color-amethystSmoke-300)] text-stone-700
+                focus-within:bg-[var(--color-amethystSmoke-400)]
+            `}>
+                {(icon && iconLeft) && <span className="text-stone-700">{icon}</span>}
                 <input className="focus:outline-none" {...props}/>
-                {(icon && !iconLeft)&& <span className="text-stone-900">{icon}</span>}
+                {(icon && !iconLeft) && <span className="text-stone-700">{icon}</span>}
             </div>
 
             {error && <p className="text-red-500">{error}</p>}

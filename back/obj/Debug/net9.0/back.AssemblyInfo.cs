74d256f11c326c94f60d2e5cc0ab83ef66924b8c
Guilherme <65544746+Guilherme-ags@users.noreply.github.com>
2025-08-19 18:11:14 -0300

    back: Files needed to compile the project
    - Files needed to compile the project

--- back/obj/Debug/net9.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net9.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62bcc41199c1031bdaa2fb207a0a21777e568b6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1152e7372594e12f65fd51f091b7b31adabd8ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
